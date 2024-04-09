1e64096b1905f9799e6a0b2f86f8a7d39e036cfe
pmwatt <prachnachai.mek@student.mahidol.edu>
2024-04-09 23:36:24 +0700

    removed unused files

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'hub.dart';
 import 'login.dart';
 import 'register.dart';
-import 'organizationDetails.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -28,18 +27,6 @@ class UWUApp extends StatelessWidget {
         '/': (context) => const Hub(),
         '/login': (context) => LoginPage(),
         '/register': (context) => RegisterPage(),
-        '/organization': (context) => AdoptionCentreDetailsPage(
-            animalName: 'Doggo Demo',
-            imageUrl:
-                'https://images.unsplash.com/photo-1561566828-2035bacf3d86?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            species: 'Demo Species',
-            breed: 'German Sheperd',
-            age: '8',
-            description: 'lorem ipsum',
-            adoptionCentreName: 'Pet Adoption 2',
-            phone: '081234567',
-            email: 'adoption@gmail.com',
-            website: 'www.adoption.com'),
       },
       theme: ThemeData(
         useMaterial3: true,
